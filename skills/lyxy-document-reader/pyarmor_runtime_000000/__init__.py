4cef5ac80cabc1870e70b3963ff006176050606e
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2026-03-16 09:21:30 +0800

    publish: lyxy-document-reader 20260316_092127

--- skills/lyxy-document-reader/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.2.3 (trial), 000000, 2026-03-11T14:24:59.985482
-from .pyarmor_runtime import __pyarmor__
---
+++ skills/lyxy-document-reader/scripts/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.3 (trial), 000000, 2026-03-16T09:21:27.947782
+from .pyarmor_runtime import __pyarmor__
